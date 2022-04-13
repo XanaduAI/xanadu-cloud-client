cc493e5814263884a821d8b94c9b0999beb2697d
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2022-04-13 15:36:20 -0400

    Format `inheritance_node_attrs` to 80 columns

--- docs/conf.py
+++ docs/conf.py
@@ -242,10 +242,9 @@ html_sidebars = {
 # Output file base name for HTML help builder.
 htmlhelp_basename = "XanaduCloudClientdoc"
 
-edit_on_github_project = "XanaduAI/xanadu-cloud-client"
-edit_on_github_branch = "main/docs"
-
-inheritance_node_attrs = dict(color="lightskyblue1", fillcolor="lightskyblue1", style="filled")
+inheritance_node_attrs = dict(
+    color="lightskyblue1", fillcolor="lightskyblue1", style="filled"
+)
 
 # -- Xanadu theme ---------------------------------------------------------
 html_theme = "xanadu"
@@ -269,4 +268,8 @@ html_theme_options = {
     "toc_marker_colour": "#78909c",
     "table_header_background_colour": "#78909c",
     "text_accent_colour": "#78909c",
+
 }
+
+edit_on_github_project = "XanaduAI/xanadu-cloud-client"
+edit_on_github_branch = "main/docs"
